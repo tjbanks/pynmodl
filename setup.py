cfdb01257ccb7c9be1bd1ebcbeace9de0251d6a7
Tyler Banks <tyler@tylerbanks.net>
2020-01-06 00:02:37 -0600

    pypi

--- setup.py
+++ setup.py
@@ -1,11 +1,11 @@
 from setuptools import setup
 
-setup(name='pynmodl',
+setup(name='pynmodlt',
       version='0.1',
-      description='NMODL parsing and compiling tools',
-      url='http://github.com/borismarin/pynmodl',
-      author='Boris Marin',
-      author_email='borismarin@gmail.com',
+      description='NMODL parsing and compiling tools, updated fork from borismarin pynmodl (http://github.com/borismarin/pynmodl)',
+      url='http://github.com/tjbanks/pynmodl',
+      author='Tyler Banks',
+      author_email='tyler@tylerbanks.net',
       license='MIT',
       packages=['pynmodl'],
       package_data={'pynmodl': ['tests/sample_mods/*.mod', 'grammar/*.tx',
